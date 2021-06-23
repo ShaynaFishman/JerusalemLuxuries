62910336cdcf9623113049cae2521a0547dddf69
ShaynaFishman <shayna.fishman@gmail.com>
2021-06-23 15:50:33 +0300

    First commit

---
+++ JerusalemLuxuries/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JerusalemLuxuries.Startup))]
+namespace JerusalemLuxuries
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
