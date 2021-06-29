6db703d9e89978c13f99294860b7eadb79d7cd11
compuskills <students@compuskills.crg>
2021-06-29 17:21:47 +0300

    Beginning of DB Connection to MVC app
    Data Source Project Created
    Description entity model included

---
+++ JerusalemLuxuries/Models/Description.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace JerusalemLuxuries.Models
+{
+    public class Description
+    {
+        [Key]
+        public int DescriptionId { get; set; }
+        public decimal Rooms { get; set; }
+        public bool Porch { get; set; }
+        public bool Lift { get; set; }
+        public int Floor { get; set; }
+        public int Bathrooms { get; set; }
+        public bool SeparateKitchenAndDining { get; set; }
+    }
+}
