6eb9b4c18897a0e0d2757f61877b9bc61d636d32
Leah Polias <78278217+Leah-Polias@users.noreply.github.com>
2021-06-29 18:02:19 +0300

    Create CardInfoModel, PaymentMethodModel, PosterModel

---
+++ JerusalemLuxuries/Models/PaymentMethodModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JerusalemLuxuries.Models
+{
+    public class PaymentMethodModel
+    {
+        [Key]
+        public int PaymentMethodId { get; set; }
+        [MaxLength(30)] public string Name { get; set; }
+        //Examples of payments types: Credit Card, Debit Card
+    }
+}
