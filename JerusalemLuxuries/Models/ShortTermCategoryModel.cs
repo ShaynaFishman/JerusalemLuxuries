29929b9549885831c487993e399039499457bb4f
C-Frenkel <77924607+C-Frenkel@users.noreply.github.com>
2021-06-29 16:46:35 -0400

    Update ShortTermCategoryModel.cs

--- JerusalemLuxuries/Models/ShortTermCategoryModel.cs
+++ JerusalemLuxuries/Models/ShortTermCategoryModel.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JerusalemLuxuries.Models
 {
     public class ShortTermCategoryModel
     {
+        [Key]
+        public int ShortTermCategoryId {get; set;}
+        [MaxLength(100)]public string Name {get; set;}
     }
 }
