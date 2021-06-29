17e17e8fe0238766b6f3c7bdb5a8b3c2954f81fc
compuskills <students@compuskills.crg>
2021-06-29 17:55:16 +0300

    Adding Entity Models
    Description
    PaymentMethod
    Property Category
    Short Term Category

---
+++ JerusalemLuxuries/Models/PaymentMethodModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JerusalemLuxuries.Models
+{
+    public class PaymentMethodModel
+    {
+        [Key]
+        public int PaymentMethodId { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+    }
+}
---
+++ JerusalemLuxuries/Models/PropertyCategoryModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace JerusalemLuxuries.Models
+{
+    public class PropertyCategoryModel
+    {
+        [Key]
+        public int PropertyCatId { get; set; }
+        
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+    }
+}
