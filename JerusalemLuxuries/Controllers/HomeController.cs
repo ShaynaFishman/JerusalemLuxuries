ef1057a8383136c38296f3f7e06e7fd16e3733d3
Leah Polias <78278217+Leah-Polias@users.noreply.github.com>
2021-06-29 15:59:59 +0300

    Conflict

--- JerusalemLuxuries/Controllers/HomeController.cs
+++ JerusalemLuxuries/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace JerusalemLuxuries.Controllers
 {
-    //comments
+    //change comment
     public class HomeController : Controller
     {
         public ActionResult Index()
