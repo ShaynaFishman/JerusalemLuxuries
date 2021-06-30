0dcbe89cd37ac01ba3646799ea875133dcde7ae0
compuskills <students@compuskills.crg>
2021-06-30 15:46:17 +0300

    Description and ShortTerm Models added

--- JerusalemLuxuries/Models/DescriptionModel.cs
+++ JerusalemLuxuries/Models/DescriptionModel.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JerusalemLuxuries.Models
 {
-    public class Description
+    public class DescriptionModel
     {
         [Key]
         public int DescriptionId { get; set; }
+
         public decimal Rooms { get; set; }
         public bool Porch { get; set; }
         public bool Lift { get; set; }
