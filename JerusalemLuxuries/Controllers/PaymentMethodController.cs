37b883eac7f8c4c9a178535770a42d42c8491a4c
rochel0108 <47180665+rochel0108@users.noreply.github.com>
2021-06-30 17:57:31 +0300

    Added Controllers

---
+++ JerusalemLuxuries/Controllers/PaymentMethodController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JerusalemLuxuries.Controllers
+{
+    public class PaymentMethodController : Controller
+    {
+        // GET: PaymentMethod
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PaymentMethod/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PaymentMethod/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PaymentMethod/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PaymentMethod/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentMethod/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PaymentMethod/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PaymentMethod/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
