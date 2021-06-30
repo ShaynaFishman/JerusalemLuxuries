56cddf091bbe329367a9a3a97f1aa15084e26992
rochel0108 <47180665+rochel0108@users.noreply.github.com>
2021-06-30 16:26:57 +0300

    Create AccountingController.cs

---
+++ JerusalemLuxuries/Controllers/AccountingController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JerusalemLuxuries.Controllers
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Accounting/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Accounting/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounting/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Accounting/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Accounting/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Accounting/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Accounting/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
